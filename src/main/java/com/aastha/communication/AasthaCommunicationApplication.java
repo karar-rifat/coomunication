5853ced79974b710f9d7bfd9d14a4e0be2a6aaac
karar rifat <kararrifat@gmail.com>
2020-08-25 02:20:51 +0600

    first commit

---
+++ src/main/java/com/aastha/communication/AasthaCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.aastha.communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AasthaCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AasthaCommunicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aastha/communication/AasthaCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aastha.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AasthaCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
